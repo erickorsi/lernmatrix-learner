6edf143b81a56f3ac5d8554ff9ceae69e7683765
Erick Hotta Orsi <33377887+erickorsi@users.noreply.github.com>
2022-09-10 20:12:37 -0300

    Update lernmatrix.py

--- lernmatrix.py
+++ lernmatrix.py
@@ -13,7 +13,10 @@ class Lernmatrix():
     def __init__(self, input_length, output_length):
         '''
         '''
-        # Creates the initial instance of the matrix
+        self.input_length = input_length
+        self.output_length = output_length
+
+        # Creates the initial instance of the matrix filled with 0s
         self.M = np.matrix(np.tile(np.zeros(input_length),(output_length,1)))
 
     def learn():
