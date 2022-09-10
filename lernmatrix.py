3a56b51a109a4dd7216f855b6a3ce6c9a1562ebb
Erick Hotta Orsi <33377887+erickorsi@users.noreply.github.com>
2022-09-10 20:07:17 -0300

    Matrix definition

--- lernmatrix.py
+++ lernmatrix.py
@@ -7,3 +7,18 @@ Created on Mon Sep 10 19:06 2022
 Lernmatrix class methods.
 """
 import numpy as np
+
+class Lernmatrix():
+
+    def __init__():
+        '''
+        '''
+
+    def learn(input_length, output_length):
+        '''
+        '''
+        M = np.matrix(np.repeat(np.zeros(input_length),output_length))
+
+    def recall():
+        '''
+        '''
