52dc20723356ff37de35fb75a29fd9494e895af7
Erick Hotta Orsi <33377887+erickorsi@users.noreply.github.com>
2022-09-11 12:10:35 -0300

    Input validation and errors

--- lernmatrix.py
+++ lernmatrix.py
@@ -22,12 +22,23 @@ class Lernmatrix():
 
     def learn(self, X, Y):
         '''
+        Learning process of Lernmatrix of a single example.
+        Follows the set of rules described in the learning phase.
+
+        Parameters
+        ----------
+        X : array or list
+            Binary sequence representing the main input.
+            Must be the same length as the Lernmatrix input length.
+            Ex.: [1,0,0,1,...,1,0,1]
+        Y : array or list
+            Binary sequence representing the expected output (associated input).
+            Must be the same length as the Lernmatrix output length.
+            Ex.: [1,0,0,1,...,1,0,1]
         '''
         # Validation of data
-        if (len(X)!=self.x_length or len(Y)!=self.y_length): # Size of inputs
-            return 1
-        if (np.any([(x!=0 and x!=1) for x in X]) or np.any([(y!=0 and y!=1) for y in Y])): # Values of inputs
-            return 2
+        _input_validation(X, self.x_length)
+        _input_validation(Y, self.y_length)
 
         # Runs inputs through Lernmatrix ruleset
         for row in range(self.y_length):
@@ -48,10 +59,7 @@ class Lernmatrix():
         '''
         '''
         # Validation of data
-        if (len(X)!=self.x_length): # Size of input
-            return 1
-        if (np.any([(x!=0 and x!=1) for x in X])): # Values of input
-            return 2
+        _input_validation(X, self.x_length)
 
         # Dot product of matrix with input
         Y_temp = np.asarray(np.dot(self.M, X)).reshape(-1)
@@ -64,7 +72,6 @@ class Lernmatrix():
 
 
 
-
 '''
 lm = Lernmatrix(4,3)
 lm.learn( X = [1,1,0,0], Y = [1,0,0] )
@@ -73,3 +80,30 @@ lm.learn( X = [0,0,0,1], Y = [0,0,1] )
 
 lm.recall( X = [0,1,0,1] )
 '''
+
+
+def _input_validation(input, length):
+        '''
+        Validates the input sequence.
+        Must be of the same length as the respective matrix size;
+        Must be composed of only 0s and 1s.
+
+        Parameters
+        ----------
+        input : array or list
+            Input sequence to be validated (can be X or Y).
+        length : int
+            Expected length of this input according to the matrix structure.
+
+        Returns
+        ----------
+        None.
+        Raises Exception if invalid.
+        '''
+        try:
+            if (len(input)!=length): # Size of inputs
+                raise ValueError("Invalid length. Sequence has size {} but matrix requires size {}".format(len(input), length))
+            if (np.any([(i!=0 and i!=1) for i in input])): # Values of inputs
+                raise ValueError("Invalid values. Sequence must be binary (composed of 0s and 1s).")
+        except TypeError:
+            raise TypeError("Invalid input type. Input must be an itterable list or array.")
